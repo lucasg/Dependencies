4ad3b070ec494ec27ac10794531e7539afa849dd
lucasg <lucas.georges@outlook.com>
2017-05-17 19:46:35 +0200

    Move Shell32 Icon extraction in a separate file

---
+++ Dependencies/Shell32IconExtractor.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Dependencies
+{
+	/// <summary> Summary description for ExtractIcon.</summary>
+    public class ExtractIcon
+    {
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        private static extern int SHGetFileInfo(
+          string pszPath,
+          int dwFileAttributes,
+          out SHFILEINFO psfi,
+          uint cbfileInfo,
+          SHGFI uFlags);
+
+        /// <summary>Maximal Length of unmanaged Windows-Path-strings</summary>
+        private const int MAX_PATH = 260;
+        /// <summary>Maximal Length of unmanaged Typename</summary>
+        private const int MAX_TYPE = 80;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct SHFILEINFO
+        {
+            public SHFILEINFO(bool b)
+            {
+                hIcon = IntPtr.Zero;
+                iIcon = 0;
+                dwAttributes = 0;
+                szDisplayName = "";
+                szTypeName = "";
+            }
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_PATH)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_TYPE)]
+            public string szTypeName;
+        };
+
+        private ExtractIcon()
+        {
+        }
+
+        [Flags]
+        enum SHGFI : int
+        {
+            /// <summary>get icon</summary>
+            Icon = 0x000000100,
+            /// <summary>get display name</summary>
+            DisplayName = 0x000000200,
+            /// <summary>get type name</summary>
+            TypeName = 0x000000400,
+            /// <summary>get attributes</summary>
+            Attributes = 0x000000800,
+            /// <summary>get icon location</summary>
+            IconLocation = 0x000001000,
+            /// <summary>return exe type</summary>
+            ExeType = 0x000002000,
+            /// <summary>get system icon index</summary>
+            SysIconIndex = 0x000004000,
+            /// <summary>put a link overlay on icon</summary>
+            LinkOverlay = 0x000008000,
+            /// <summary>show icon in selected state</summary>
+            Selected = 0x000010000,
+            /// <summary>get only specified attributes</summary>
+            Attr_Specified = 0x000020000,
+            /// <summary>get large icon</summary>
+            LargeIcon = 0x000000000,
+            /// <summary>get small icon</summary>
+            SmallIcon = 0x000000001,
+            /// <summary>get open icon</summary>
+            OpenIcon = 0x000000002,
+            /// <summary>get shell size icon</summary>
+            ShellIconSize = 0x000000004,
+            /// <summary>pszPath is a pidl</summary>
+            PIDL = 0x000000008,
+            /// <summary>use passed dwFileAttribute</summary>
+            UseFileAttributes = 0x000000010,
+            /// <summary>apply the appropriate overlays</summary>
+            AddOverlays = 0x000000020,
+            /// <summary>Get the index of the overlay in the upper 8 bits of the iIcon</summary>
+            OverlayIndex = 0x000000040,
+        }
+
+        /// <summary>
+        /// Get the associated Icon for a file or application, this method always returns
+        /// an icon.  If the strPath is invalid or there is no idonc the default icon is returned
+        /// </summary>
+        /// <param name="strPath">full path to the file</param>
+        /// <param name="bSmall">if true, the 16x16 icon is returned otherwise the 32x32</param>
+        /// <returns></returns>
+        public static Icon GetIcon(string strPath, bool bSmall)
+        {
+            SHFILEINFO info = new SHFILEINFO(true);
+            int cbFileInfo = Marshal.SizeOf(info);
+            SHGFI flags;
+            if (bSmall)
+                flags = SHGFI.Icon | SHGFI.SmallIcon | SHGFI.UseFileAttributes;
+            else
+                flags = SHGFI.Icon | SHGFI.LargeIcon | SHGFI.UseFileAttributes;
+
+            SHGetFileInfo(strPath, 256, out info, (uint)cbFileInfo, flags);
+            return Icon.FromHandle(info.hIcon);
+        }
+    }
+
+    public class ImageToHeaderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string Filename = (string)value;
+
+            if (Filename == null)
+                return null;
+
+            Icon icon = ExtractIcon.GetIcon(Filename, true);
+
+            if (icon != null)
+            {
+               
+                return System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                            icon.Handle,
+                            new Int32Rect(0, 0, icon.Width, icon.Height),
+                            BitmapSizeOptions.FromEmptyOptions()); 
+            }
+            
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
