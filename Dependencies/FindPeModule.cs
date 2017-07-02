f9e92a3849afb46cddcdb3c495352c14d6424a4d
lucasg <lucas.georges@outlook.com>
2017-07-02 12:15:15 +0200

    [Dependencies] Look for modules in KnownDlls list
    Also documented the simulated dll search order.

--- Dependencies/FindPeModule.cs
+++ Dependencies/FindPeModule.cs
@@ -26,35 +26,58 @@ namespace Dependencies
 
         // default search order : 
         // https://msdn.microsoft.com/en-us/library/windows/desktop/ms682586(v=vs.85).aspx
-        public static string FindPeFromDefault(string ModuleName, string RootPeFolder)
+        // 
+        // if (SafeDllSearchMode) {
+        //      0. KnownDlls list
+        //      1. Loaded PE folder
+        //      2. C:\Windows\(System32 | SysWow64 )
+        //      3. 16-bit system directory   <-- ignored
+        //      4. C:\Windows
+        //      5. %pwd%
+        //      6. AppDatas
+        //      7. Sxs manifests
+        public static string FindPeFromDefault(string ModuleName, string RootPeFolder, bool Wow64Dll = false)
         {
             string FoundPePath = null;
+            Environment.SpecialFolder WindowsSystemFolder = (Wow64Dll) ?
+                Environment.SpecialFolder.SystemX86 :
+                Environment.SpecialFolder.System;
+            String WindowsSystemFolderPath = Environment.GetFolderPath(WindowsSystemFolder);
 
-            // Look in application folder
-            FoundPePath = FindPeFromPath(ModuleName, new List<string>(new string[] { RootPeFolder }));
-    
-            if (FoundPePath != null)
-                return FoundPePath;
-
-            // Look in system folders
-            List<String> SystemFolders = new List<string>(new string[] {
-                Environment.GetFolderPath(Environment.SpecialFolder.System),
-                Environment.GetFolderPath(Environment.SpecialFolder.SystemX86)
-                }
-            );
-            FoundPePath = FindPeFromPath(ModuleName, SystemFolders);
-    
-            if (FoundPePath != null)
-                return FoundPePath;
-
-            // Look in well-known dll
+            // 0. Look in well-known dlls list
             // HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\KnownDLLs
             // https://blogs.msdn.microsoft.com/larryosterman/2004/07/19/what-are-known-dlls-anyway/
+            String KnownDll = Phlib.GetKnownDlls(Wow64Dll).Find(x => x == ModuleName);
+            if (KnownDll != null)
+            {
+                return Path.Combine(WindowsSystemFolderPath, KnownDll);
+            }
 
+            // 1. Look in application folder
+            FoundPePath = FindPeFromPath(ModuleName, new List<string>(new string[] { RootPeFolder }), Wow64Dll);
+            if (FoundPePath != null)
+            {
+                return FoundPePath;
+            }
 
-            // Look in local app data (check for python for exemple)
+            // {2-3-4}. Look in system folders
+            List <String> SystemFolders = new List<string>(new string[] {
+                WindowsSystemFolderPath,
+                Environment.GetFolderPath(Environment.SpecialFolder.Windows)
+                }
+            );
 
-            // Look in Sxs manifest (copious reversing needed)
+            FoundPePath = FindPeFromPath(ModuleName, SystemFolders, Wow64Dll);
+            if (FoundPePath != null)
+            {
+                return FoundPePath;
+            }
+                
+            // 5. Look in %pwd%
+                
+            // 6. Look in local app data (check for python for exemple)
+
+            // 7. Look in Sxs manifest (copious reversing needed)
 
             return null;
         }
