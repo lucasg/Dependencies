d9da2b3378de79197b8d2633fbe5ba4a8a1a5984
lucasg <lucas.georges@outlook.com>
2018-10-01 19:04:48 +0200

    [test] add a demangling test executable

---
+++ test/demangler-test/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ClrPh;
+
+namespace Dependencies
+{
+    namespace Test
+    { 
+        class Program
+        {
+            static bool TestKnownInputs()
+            {
+                return true;
+            }
+
+            static bool TestFilepath(string Filepath)
+            {
+                PhSymbolProvider SymPrv = new PhSymbolProvider();
+
+                PE Pe = new PE(Filepath);
+                if (!Pe.Load())
+                {
+                    Console.Error.WriteLine("[x] Could not load file {0:s} as a PE", Filepath);
+                    return false;
+                }
+
+                foreach (PeExport Export in Pe.GetExports())
+                {
+                    if (Export.Name.Length > 0)
+                        Console.WriteLine("\t Export : {0:s} -> {1:s}", Export.Name, SymPrv.UndecorateName(Export.Name));
+                }
+
+                foreach (PeImportDll DllImport in Pe.GetImports())
+                {
+                    foreach (PeImport Import in DllImport.ImportList)
+                    {
+                        if (!Import.ImportByOrdinal)
+                        {
+                            Console.WriteLine("\t Import from {0:s} : {1:s} -> {2:s}", DllImport.Name, Import.Name, SymPrv.UndecorateName(Import.Name));
+                        }
+                    }
+                }
+
+                return true;
+            }
+
+            static void Main(string[] args)
+            {
+                // always the first call to make
+                Phlib.InitializePhLib();
+
+                if (args.Length == 0)
+                {
+                    TestKnownInputs();
+                    return;
+                }
+
+                TestFilepath(args[0]);
+            }
+        }
+    }
+}
---
+++ test/demangler-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("demangler-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("demangler-test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("24f9e0c0-ca6e-4565-a9e2-88602dd9f18a")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
