ad3c7224be37b6f1eff7523523f7ab6ded085743
lucasg <lucas.georges@outlook.com>
2018-10-10 21:43:26 +0200

    [test] Add executable to test binary cache loading

---
+++ test/binarycache-test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Dependencies.ClrPh;
+
+namespace Dependencies
+{
+    namespace Test
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // always the first call to make
+                Phlib.InitializePhLib();
+
+                // Redirect debug log to the console
+                Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+                Debug.AutoFlush = true;
+
+                BinaryCache.Instance.Load();
+
+                foreach ( var peFilePath in args )
+                {
+                    PE Pe = BinaryCache.LoadPe(peFilePath);
+                }
+
+
+                BinaryCache.Instance.Unload();
+            }
+        }
+    }
+}
