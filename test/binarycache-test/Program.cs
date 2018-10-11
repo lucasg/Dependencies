fca5ae088e2c3864cc1e728fba8cfbc7b434bf95
lucasg <lucas.georges@outlook.com>
2018-10-11 12:53:14 +0200

    [test] Use NDes.options for binarycache test executable

--- test/binarycache-test/Program.cs
+++ test/binarycache-test/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+
 using Dependencies.ClrPh;
+using NDesk.Options;
 
 namespace Dependencies
 {
@@ -8,20 +11,48 @@ namespace Dependencies
     {
         class Program
         {
+            static void ShowHelp(OptionSet p)
+            {
+                Console.WriteLine("Usage: binarycache [options] <FILES_TO_LOAD>");
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                p.WriteOptionDescriptions(Console.Out);
+            }
+
             static void Main(string[] args)
             {
+                bool is_verbose = false;
+                bool show_help = false;
+
+                OptionSet opts = new OptionSet() {
+                            { "v|verbose", "redirect debug traces to console", v => is_verbose = v != null },
+                            { "h|help",  "show this message and exit", v => show_help = v != null },
+                        };
+
+                List<string> eps = opts.Parse(args);
+
+                if (show_help)
+                {
+                    ShowHelp(opts);
+                    return;
+                }
+
+                if (is_verbose)
+                {
+                    // Redirect debug log to the console
+                    Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+                    Debug.AutoFlush = true;
+                }
+
                 // always the first call to make
                 Phlib.InitializePhLib();
 
-                // Redirect debug log to the console
-                Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
-                Debug.AutoFlush = true;
-
                 BinaryCache.Instance.Load();
 
-                foreach ( var peFilePath in args )
+                foreach ( var peFilePath in eps)
                 {
                     PE Pe = BinaryCache.LoadPe(peFilePath);
+                    Console.WriteLine("Loaded PE file : {0:s}", Pe.Filepath);
                 }
 
 
