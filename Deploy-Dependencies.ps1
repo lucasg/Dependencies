d1819c824e7b73c985a380ab5adae8d6ea1f8ff8
lucasg <lucas.georges@outlook.com>
2018-10-18 11:37:20 +0200

    Improve deploy script

--- Deploy-Dependencies.ps1
+++ Deploy-Dependencies.ps1
@@ -1,22 +1,46 @@
-# Download external dependencies like peview
-$PEVIEW_BIN="";
+function Get-PeviewBinary {
+  param(
+    [String] $Url,
+    [String] $Hash
+  )
 
-Push-Location;
-New-Item -ItemType Directory -Force -Path "tmp";
-cd tmp;
-&wget "https://github.com/processhacker2/processhacker2/releases/download/v2.39/processhacker-2.39-bin.zip" -OutFile processhacker-2.39-bin.zip;
-$PhArchiveHash = (Get-FileHash -Algorithm SHA256 -Path ./processhacker-2.39-bin.zip).Hash;
-if ($PhArchiveHash -eq "2afb5303e191dde688c5626c3ee545e32e52f09da3b35b20f5e0d29a418432f5") {
-  &7z.exe x ./processhacker-2.39-bin.zip $($env:platform)/peview.exe
-  $PEVIEW_BIN = (Resolve-Path ./$($env:platform)/peview.exe).Path;
+  Push-Location;
+  $PeviewBinaryFile = "";
+
+  # use temporary folder for download
+  New-Item -ItemType Directory -Force -Path "tmp";
+  Set-Location "tmp";
+
+  &wget $Url -OutFile "processhacker-2.39-bin.zip";
+  $PhArchiveHash = (Get-FileHash -Algorithm SHA256 -Path "./processhacker-2.39-bin.zip").Hash;
+
+  if ($PhArchiveHash -eq "2afb5303e191dde688c5626c3ee545e32e52f09da3b35b20f5e0d29a418432f5") {
+    &7z.exe x ./processhacker-2.39-bin.zip $($env:platform)/peview.exe
+    $PeviewBinaryFile = (Resolve-Path ./$($env:platform)/peview.exe).Path;
+  }
+
+  Pop-Location;
+
+  return $PeviewBinaryFile;
 }
-Pop-Location;
+
+# Download external dependencies like peview
+$ProcessHackerReleaseUrl = "https://github.com/processhacker2/processhacker2/releases/download/v2.39/processhacker-2.39-bin.zip";
+$PeviewReleaseHash = "2afb5303e191dde688c5626c3ee545e32e52f09da3b35b20f5e0d29a418432f5";
+$PEVIEW_BIN= Get-PeviewBinary -Url $ProcessHackerReleaseUrl -Hash $PeviewReleaseHash;
+
+if (-not $PEVIEW_BIN)
+{
+  Write-Error "[x] Peview binary has not correctly been downloaded."
+}
+
 
 # Bundling dbghelp.dll along for undecorating names
-$DbgHelpDll="";
-if (Test-Path "$env:SystemRoot\System32\dbghelp.dll") {
-  $DbgHelpDll=(Resolve-Path $env:SystemRoot\System32\dbghelp.dll).Path;
+$DbgHelpDll="$env:SystemRoot\System32\dbghelp.dll";
+if (Test-Path $DbgHelpDll) {
+  $DbgHelpDll=(Resolve-Path $DbgHelpDll).Path;
 }
+
 
 # Creating output directory
 New-Item -ItemType Directory -Force -Path "output";
@@ -31,6 +55,7 @@ Write-Host "Test if the binary (and the underlying lib) actually works"
 &"$BINPATH/Dependencies.exe" -sxsentries "$($env:windir)/System32/ctfmon.exe" 
 
 # &"$BINPATH/demangler-test.exe"
+Write-Host "Tests done."
 
 Write-Host "Zipping everything"
 &7z.exe a Dependencies_$($env:platform)_$($env:CONFIGURATION).zip $BINPATH/*.dll $BINPATH/*.exe $BINPATH/*.config $BINPATH/*.pdb $PEVIEW_BIN $DbgHelpDll;
