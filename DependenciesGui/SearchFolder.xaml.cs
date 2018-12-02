8ead7ef45451ac93448d381c62c1f002a265efbd
lucasg <lucas.georges@outlook.com>
2018-12-02 17:49:30 +0100

    [gui] Correctly implement user provided search folder dialog

--- DependenciesGui/SearchFolder.xaml.cs
+++ DependenciesGui/SearchFolder.xaml.cs
@@ -11,32 +11,67 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.ComponentModel;
 using System.Windows.Forms;
 
 namespace Dependencies
 {
+	public class SearchFolderItem : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChanged(string info)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(info));
+			}
+		}
+
+		public string Folder {
+			get { return folder; }
+			set { folder = value;  OnPropertyChanged("Folder"); }
+		}
+
+		public bool Dummy { get; set; }
+
+		private string folder;
+	}
+
 	/// <summary>
 	/// Logique d'interaction pour SearchFolder.xaml
 	/// </summary>
 	public partial class SearchFolder : Window
 	{
 		private DependencyWindow _SelectedItem;
-		private ObservableCollection<string> _CustomSearchFolders;
+		private ObservableCollection<SearchFolderItem> _CustomSearchFolders;
 		private string _working_directory;
 
 		public SearchFolder(DependencyWindow SelectedItem)
 		{
 			_SelectedItem = SelectedItem;
 			_working_directory = SelectedItem.RootFolder;
-			_CustomSearchFolders = new ObservableCollection<string>(SelectedItem.CustomSearchFolders);
-			
+			_CustomSearchFolders = new ObservableCollection<SearchFolderItem>();
+			foreach (var item in SelectedItem.CustomSearchFolders)
+			{
+				_CustomSearchFolders.Add(
+					new SearchFolderItem()
+					{
+						Folder = item,
+						Dummy = false
+					}
+				);
+			}
+
+			EnsureSearchFolderHasDummyEntry();
+
 			// bind window to observable collections
 			this.DataContext = this; 
 			InitializeComponent();
 		}
 
-		public ObservableCollection<string> SearchFolders
+		public ObservableCollection<SearchFolderItem> SearchFolders
 		{
 			get
 			{
@@ -48,6 +83,50 @@ namespace Dependencies
 		{
 			get { return _working_directory; }
 			set { _working_directory = value; this.WorkingDirectoryTextBox.Text = value;}
+		}
+
+		private void OnChangeSearchFolder(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Controls.Button selectedButton = sender as System.Windows.Controls.Button;
+			SearchFolderItem selectedItem = (SearchFolderItem) (selectedButton.DataContext as System.Windows.Controls.ListBoxItem).Content;
+
+			FolderBrowserDialog InputFileNameDlg = new FolderBrowserDialog();
+			if (InputFileNameDlg.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+				return;
+
+			var selectedPath = InputFileNameDlg.SelectedPath;
+
+
+			selectedItem.Folder = selectedPath;
+			selectedItem.Dummy = false;
+			
+
+			EnsureSearchFolderHasDummyEntry();
+		}
+
+		private void OnRemoveSearchFolder(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Controls.Button selectedButton = sender as System.Windows.Controls.Button;
+			SearchFolderItem selectedItem = (SearchFolderItem)(selectedButton.DataContext as System.Windows.Controls.ListBoxItem).Content;
+
+			_CustomSearchFolders.Remove(selectedItem);
+
+			EnsureSearchFolderHasDummyEntry();
+		}
+
+		private void EnsureSearchFolderHasDummyEntry()
+		{
+			// add a dummy entry if necessary
+			if (_CustomSearchFolders.Count == 0 || !_CustomSearchFolders.Last().Dummy)
+			{
+				_CustomSearchFolders.Add(
+					new SearchFolderItem()
+					{
+						Folder = null,
+						Dummy = true
+					}
+				);
+			}
 		}
 
 		private void OnBinaryWorkindDirectoryChange(object sender, RoutedEventArgs e)
@@ -71,14 +150,19 @@ namespace Dependencies
 
 		private void OnValidate(object sender, RoutedEventArgs e)
 		{
+			var nonDummySearchFolders = _CustomSearchFolders.ToList()
+												.FindAll(item => !item.Dummy)
+												.Select(i => i.Folder)
+												.ToList();
+
 			// do not launch analysis again if there is no modifications
-			bool searchFoldersChanged = (_SelectedItem.CustomSearchFolders != _CustomSearchFolders.ToList()) || (WorkingDirectory != _SelectedItem.WorkingDirectory);
+			bool searchFoldersChanged = (_SelectedItem.CustomSearchFolders != nonDummySearchFolders) || (WorkingDirectory != _SelectedItem.WorkingDirectory);
 			
 			this.Close();
 
 			if (searchFoldersChanged)
 			{
-				_SelectedItem.CustomSearchFolders = _CustomSearchFolders.ToList();
+				_SelectedItem.CustomSearchFolders = nonDummySearchFolders;
 				_SelectedItem.WorkingDirectory = WorkingDirectory;
 
 				// Force refresh
@@ -116,7 +200,21 @@ namespace Dependencies
 				{
 					if (Directory.Exists(FolderPath))
 					{
-						_CustomSearchFolders.Add(FolderPath);
+						if (_CustomSearchFolders.Last().Dummy)
+						{
+							_CustomSearchFolders.Last().Folder = FolderPath;
+							_CustomSearchFolders.Last().Dummy = false;
+						}
+						else
+						{ 
+							_CustomSearchFolders.Add(new SearchFolderItem()
+							{
+								Folder = FolderPath,
+								Dummy = false
+							});
+						}
+
+						EnsureSearchFolderHasDummyEntry();
 					}
 				}
 			}
