69d05a7e84229f986813697f2e8216afa2ac5319
Mark Jansen <mark.jansen@reactos.org>
2020-10-15 18:27:45 +0200

    [about] Add on-demand version check

--- DependenciesGui/About.xaml.cs
+++ DependenciesGui/About.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Navigation;
 
 namespace Dependencies
@@ -27,6 +31,52 @@ namespace Dependencies
             {
                 return Assembly.GetEntryAssembly().GetName().Version.ToString();
             }
+        }
+
+        private async void Uri_CheckUpdates(object sender, RequestNavigateEventArgs e)
+        {
+            string version = await GetLatestVersion("https://github.com/lucasg/Dependencies/releases/latest");
+            UpdateCheck.Inlines.Clear();
+            UpdateCheck.Inlines.Add("Latest version: ");
+            var link = new Hyperlink()
+            {
+                NavigateUri = new Uri(version)
+            };
+            link.Inlines.Add(version);
+            link.RequestNavigate += Uri_RequestNavigate;
+            UpdateCheck.Inlines.Add(link);
+        }
+
+        // Based on https://stackoverflow.com/a/28424940/4928207
+        private async Task<string> GetLatestVersion(string url)
+        {
+            try
+            {
+                var req = (HttpWebRequest)HttpWebRequest.Create(url);
+                req.Method = "HEAD";
+                req.AllowAutoRedirect = false;
+                using (var resp = (HttpWebResponse)await req.GetResponseAsync())
+                {
+                    switch (resp.StatusCode)
+                    {
+                        case HttpStatusCode.OK:
+                            return url;
+                        case HttpStatusCode.Redirect:
+                        case HttpStatusCode.MovedPermanently:
+                        case HttpStatusCode.RedirectKeepVerb:
+                        case HttpStatusCode.RedirectMethod:
+                            if (resp.Headers["Location"] == null)
+                                return url;
+                            return resp.Headers["Location"];
+                        default:
+                            return url;
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return url;
         }
     }
 }
