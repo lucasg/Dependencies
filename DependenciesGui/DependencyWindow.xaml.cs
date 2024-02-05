de6f41c9a62e41b46899556bd696d6cbd6b83900
Jan Michal <jan.majkl@seznam.cz>
2024-02-05 06:03:33 +0100

    Copy file path and show in file explorer implemented

--- DependenciesGui/DependencyWindow.xaml.cs
+++ DependenciesGui/DependencyWindow.xaml.cs
@@ -1346,6 +1346,28 @@ namespace Dependencies
             }
         }
 
+        private void CopyFilePath_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            ModuleTreeViewItem Source = e.Source as ModuleTreeViewItem;
+            String SelectedModuleName = Source.GetTreeNodeHeaderName(Dependencies.Properties.Settings.Default.FullPath);
+            if (Source == null)
+                return;
+
+            Clipboard.SetText(SelectedModuleName);
+        }
+
+        private void OpenInExplorer_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            ModuleTreeViewItem Source = e.Source as ModuleTreeViewItem;
+            if (Source == null)
+                return;
+
+            String SelectedModuleName = Source.GetTreeNodeHeaderName(Dependencies.Properties.Settings.Default.FullPath);
+            String commandParameter = "/select,\"" + SelectedModuleName + "\"";
+
+            Process.Start("explorer.exe", commandParameter);
+        }
+
         private void ExpandAllParentNode(ModuleTreeViewItem Item)
         {
             if (Item != null)
